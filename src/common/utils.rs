f8dda5b9b3f9bac68f5d499ae0aa9a3bc946cb9d
Sasha Guérin--Loison <sasha.guerinloison@gmail.com>
2026-04-08 00:08:10 +0200

    Completed first version of the UI

--- src/common/utils.rs
+++ src/common/utils.rs
@@ -1,7 +1,12 @@
-use chrono::{DateTime, Local, Timelike};
-use colored::ColoredString;
+use std::{fs::{File, OpenOptions}, io::{BufReader, Write}, sync::Mutex};
+
+use chrono::{DateTime, Datelike, Local, Timelike};
+use colored::{ColoredString, Colorize};
 
 use crate::common::constants::*;
+
+
+static LOG_FILE_LOCK: Mutex<()> = Mutex::new(());
 
 
 pub fn char6(ord: u8) -> char {
@@ -38,6 +43,16 @@ pub fn datetime_to_slots_idx(datetime: Option<DateTime<Local>>) -> [u16; 2] {
 
 
 pub fn log(msg: ColoredString) -> () {
-    let slots: [u16; 2] = datetime_to_slots_idx(None);
-    println!("[{}, {}] : {}", slots[0], slots[1], msg);
+    let cdt: DateTime<Local> = get_current_datetime();
+    let slots: [u16; 2] = datetime_to_slots_idx(Some(cdt));
+    let log_msg: String = format!("({}, {}), {}/{}/{} {}h:{}mn:{}s :\n\t{}\n", slots[0], slots[1], cdt.day(), cdt.month(), cdt.hour(), cdt.hour(), cdt.minute(), cdt.second(), msg.clone());
+    let file_log_msg: String = format!("\n        {}\n({}, {}), {}/{}/{} {}h:{}mn:{}s:", msg.clear(), slots[0], slots[1], cdt.day(), cdt.month(), cdt.hour(), cdt.hour(), cdt.minute(), cdt.second());
+
+    let lock: std::sync::MutexGuard<'_, ()> = LOG_FILE_LOCK.lock().unwrap();
+
+    let mut log_file: File = OpenOptions::new().create(true).append(true).open("./logs.log").unwrap();
+
+    let _ = writeln!(log_file, "{file_log_msg}");
+
+    println!("{log_msg}");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::sync::Arc;
+use std::{fs::File, io::BufReader, rc::Rc, sync::Arc};
 
-use slint::ToSharedString;
+use rev_lines::RevLines;
+use slint::{ModelRc, SharedString, ToSharedString, VecModel};
 
 use colored::*;
 
-use crate::{boat::Boat, boat_info::BoatInfo, common::utils::log};
+use crate::{boat::Boat, boat_info::{BoatInfo, NavigationData, StaticData, VoyageData}, common::utils::log};
 
 mod common;
 mod ais;
@@ -22,12 +23,28 @@ mod display;
 slint::include_modules!();
 
 
+fn get_last_logs_entries(count: usize) -> ModelRc<SharedString> {
+    let file: File = File::open("./logs.log").unwrap();
+    let rev_lines: RevLines<BufReader<File>> = RevLines::new(BufReader::new(file));
+
+    let unwraped_logs: VecModel<SharedString> = VecModel::<SharedString>::default();
+
+    for entry in rev_lines.take(count) {
+        if let Ok(line) = entry {
+            unwraped_logs.push(line.into());
+        }
+    }
+
+    Rc::new(unwraped_logs).clone().into()
+}
+
+
 #[tokio::main]
 async fn main() {
-    let ui = AppWindow::new().expect("REASON");
-    let ui_handle = ui.as_weak();
+    let ui: AppWindow = AppWindow::new().expect("REASON");
+    let ui_handle: slint::Weak<AppWindow> = ui.as_weak();
 
-    let boat = Boat::init().await;
+    let boat: Boat = Boat::init().await;
 
     let boat_info: Arc<BoatInfo> = boat.info();
 
@@ -35,7 +52,7 @@ async fn main() {
         boat.start().await;
     });
 
-    ui.on_request_close(move || {
+    ui.on_close(move || {
         log("Extinction du système...".yellow());
         slint::quit_event_loop().expect("Erreur lors de la fermeture");
     });
@@ -45,15 +62,50 @@ async fn main() {
 
         loop {
             interval.tick().await;
-            
-            let mmsi: u32 = boat_info.get_static_data().mmsi;
-            let name: String = boat_info.get_static_data().name;
 
-            let ui_handle_clone = ui_handle.clone();
+            let static_data: StaticData = boat_info.get_static_data();
+            let voyage_data: VoyageData = boat_info.get_voyage_data();
+            let nav_data: NavigationData = boat_info.get_navigation_data();
+            
+            let name: String = static_data.name;
+            let mmsi: u32 = static_data.mmsi;
+            let imo: u32 = static_data.imo_number;
+            let boat_type: u8 = static_data.type_of_ship_and_cargo_type;
+            let longitude: u32 = nav_data.longitude;
+            let latitude: u32 = nav_data.latitude;
+            let heading: u16 = nav_data.true_heading;
+            let speed: u16 = nav_data.speed_over_ground;
+            let turn_rate: i8 = nav_data.rate_of_turn;
+            let destination: String = voyage_data.destination;
+            let eta_month: u8 = voyage_data.eta_month;
+            let eta_day: u8 = voyage_data.eta_day;
+            let eta_hour: u8 = voyage_data.eta_hour;
+            let eta_minute: u8 = voyage_data.eta_minute;
+
+
+            let ui_handle_clone: slint::Weak<AppWindow> = ui_handle.clone();
 
             let _ = slint::invoke_from_event_loop(move || {
                 if let Some(ui) = ui_handle_clone.upgrade() {
-                    //ui.set_boat_mmsi(mmsi as i32);
+                    let boat_data = ui.global::<BoatData>();
+                    let logs: ModelRc<SharedString> = get_last_logs_entries(300);
+
+                    boat_data.set_boat_name(name.to_shared_string());
+                    boat_data.set_boat_mmsi(mmsi as i32);
+                    boat_data.set_boat_imo(imo as i32);
+                    boat_data.set_boat_type(boat_type as i32);
+                    boat_data.set_boat_longitude(longitude as i32);
+                    boat_data.set_boat_latitude(latitude as i32);
+                    boat_data.set_boat_heading(heading as i32);
+                    boat_data.set_boat_speed(speed as i32);
+                    boat_data.set_boat_turn_rate(turn_rate as i32);
+                    boat_data.set_boat_destination(destination.to_shared_string());
+                    boat_data.set_boat_eta_month(eta_month as i32);
+                    boat_data.set_boat_eta_day(eta_day as i32);
+                    boat_data.set_boat_eta_hour(eta_hour as i32);
+                    boat_data.set_boat_eta_minute(eta_minute as i32);
+
+                    boat_data.set_log_msgs(logs);
                 }
             });
         }

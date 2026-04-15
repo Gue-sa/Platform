52607157966468b06daa5de3ea77501391f938a7
Sasha Guérin--Loison <sasha.guerinloison@gmail.com>
2026-04-15 03:15:54 +0200

    Save for fusion

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
 use std::thread::park;
 
-use crate::{radio_frequency::RadioFrequency, shared::common::{constants::{C87B_EM_PORT, C87B_REC_PORT, C88B_EM_PORT, C88B_REC_PORT, GPS_EM_PORT, GPS_REC_PORT, SATCOM_EM_PORT, SATCOM_REC_PORT}, types::*}};
+use shared::common::{
+    constants::{
+        C87B_EM_PORT, C87B_REC_PORT, C88B_EM_PORT, C88B_REC_PORT, GPS_EM_PORT, GPS_REC_PORT,
+        SATCOM_EM_PORT, SATCOM_REC_PORT,
+    },
+    types::Channel,
+};
 
+use crate::radio_frequency::RadioFrequency;
 
+mod clients_registry;
 mod common;
 mod radio_frequency;
-mod clients_registry;
-mod shared;
-
 
 #[tokio::main]
 async fn main() {
-    let freq_c87b: RadioFrequency = RadioFrequency::init(Channel::C87B, C87B_EM_PORT, C87B_REC_PORT).await;
-    let freq_c88b: RadioFrequency = RadioFrequency::init(Channel::C88B, C88B_EM_PORT, C88B_REC_PORT).await;
-    let freq_gps: RadioFrequency = RadioFrequency::init(Channel::GPS, GPS_EM_PORT, GPS_REC_PORT).await;
-    let freq_satcom: RadioFrequency = RadioFrequency::init(Channel::SATCOM, SATCOM_EM_PORT, SATCOM_REC_PORT).await;
-    
+    let freq_c87b: RadioFrequency =
+        RadioFrequency::init(Channel::C87B, C87B_EM_PORT, C87B_REC_PORT).await;
+    let freq_c88b: RadioFrequency =
+        RadioFrequency::init(Channel::C88B, C88B_EM_PORT, C88B_REC_PORT).await;
+    let freq_gps: RadioFrequency =
+        RadioFrequency::init(Channel::GPS, GPS_EM_PORT, GPS_REC_PORT).await;
+    let freq_satcom: RadioFrequency =
+        RadioFrequency::init(Channel::SATCOM, SATCOM_EM_PORT, SATCOM_REC_PORT).await;
+
     freq_c87b.start();
     freq_c88b.start();
     freq_gps.start();
--- src/radio_frequency.rs
+++ src/radio_frequency.rs
@@ -2,10 +2,13 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 use dashmap::DashSet;
 
+use shared::{
+    bitpacker::BitPacker,
+    common::{constants::GPS_EM_PORT, types::Channel},
+};
 use tokio::net::UdpSocket;
 
-use crate::{clients_registry::ClientsRegistry, common::constants::HARBOURMASTER_IP, shared::{bitpacker::BitPacker, common::{constants::*, types::*}}};
-
+use crate::{clients_registry::ClientsRegistry, common::constants::HARBOURMASTER_IP};
 
 pub struct RadioFrequency {
     pub channel: Channel,
@@ -13,44 +16,58 @@ pub struct RadioFrequency {
     pub clients: ClientsRegistry,
     pub em_port: u16,
     pub rec_port: u16,
-    pub pending_gps_clients: DashSet<IpAddr>
+    pub pending_gps_clients: DashSet<IpAddr>,
 }
-
 
 impl RadioFrequency {
     pub async fn init(channel: Channel, em_port: u16, rec_port: u16) -> Self {
         Self {
             channel: channel,
-            socket: UdpSocket::bind(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0,0,0,0)), rec_port)).await.unwrap(),
+            socket: UdpSocket::bind(SocketAddr::new(
+                IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
+                rec_port,
+            ))
+            .await
+            .unwrap(),
             clients: ClientsRegistry::init(),
             em_port: em_port,
             rec_port: rec_port,
-            pending_gps_clients: DashSet::new()
+            pending_gps_clients: DashSet::new(),
         }
     }
-
 
     pub async fn relay(&self, buf: &[u8]) {
         for client_ip in self.clients.get() {
-            let _ = self.socket.send_to(buf, SocketAddr::new(client_ip, self.em_port)).await;
+            self
+                .socket
+                .send_to(buf, SocketAddr::new(client_ip, self.em_port))
+                .await;
         }
     }
 
-
     pub async fn handle_gps_request(&self, msg: BitPacker) -> () {
-        let _ = self.socket.send_to(msg.bits(), SocketAddr::new(HARBOURMASTER_IP, GPS_EM_PORT)).await;
-        self.pending_gps_clients.insert(IpAddr::V4(Ipv4Addr::from_bits(msg.extract_int::<u32>(None, None).unwrap())));
+        self
+            .socket
+            .send_to(msg.bits(), SocketAddr::new(HARBOURMASTER_IP, GPS_EM_PORT))
+            .await;
+        self.pending_gps_clients
+            .insert(IpAddr::V4(Ipv4Addr::from_bits(
+                msg.extract_int::<u32>(None, None).unwrap(),
+            )));
     }
-
 
     pub async fn handle_gps_response(&self, msg: BitPacker) -> () {
-        let client: IpAddr = IpAddr::V4(Ipv4Addr::from_bits(msg.extract_int::<u32>(None, Some(31)).unwrap()));
+        let client: IpAddr = IpAddr::V4(Ipv4Addr::from_bits(
+            msg.extract_int::<u32>(None, Some(31)).unwrap(),
+        ));
         let data: BitPacker = msg.slice(Some(32), None).unwrap();
 
-        let _ = self.socket.send_to(data.bits(), SocketAddr::new(client, GPS_EM_PORT)).await;
+        self
+            .socket
+            .send_to(data.bits(), SocketAddr::new(client, GPS_EM_PORT))
+            .await;
         self.pending_gps_clients.remove(&client);
     }
-
 
     pub fn start(self) {
         tokio::spawn(async move {
@@ -62,7 +79,7 @@ impl RadioFrequency {
                 let (size, source) = result.unwrap();
                 let msg: BitPacker = BitPacker::from_slice(&buf[..size], Some(size * 8));
 
-                println!("{:?}", msg);
+                println!("{}\n", msg.to_bin_string());
 
                 self.clients.register_client(source.ip());
 

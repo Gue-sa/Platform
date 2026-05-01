a3e7100b4626b3d8673e7178fe7a9858cf541634
Gue-sa <sasha.guerinloison@gmail.com>
2026-05-01 21:45:28 +0200

    Fixed build-releases.sh
    The script was lacking --bin launcher for aarch64 release build

--- build-releases.sh
+++ build-releases.sh
@@ -2,4 +2,4 @@
 
 cargo build --release --features "arch-based"
 cross build --target x86_64-unknown-linux-gnu --release --features "debian-based"
-cross build --target aarch64-unknown-linux-gnu --release --bin boat --bin server --features "rasp-based"
+cross build --target aarch64-unknown-linux-gnu --release --bin boat --bin server --bin launcher --features "rasp-based"
